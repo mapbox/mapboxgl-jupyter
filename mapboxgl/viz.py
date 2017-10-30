2609a8c7ff018390640f524313aa85aff88c257e
Ryan Baumann <rsbaumann@gmail.com>
2017-10-29 17:05:34 -0700

    update to gl js v0.41.0

--- mapboxgl/viz.py
+++ mapboxgl/viz.py
@@ -6,7 +6,7 @@ from IPython.core.display import HTML, display
 from mapboxgl.errors import TokenError
 from mapboxgl import templates
 
-GL_JS_VERSION = 'v0.40.1'
+GL_JS_VERSION = 'v0.41.0'
 
 
 class CircleViz(object):
