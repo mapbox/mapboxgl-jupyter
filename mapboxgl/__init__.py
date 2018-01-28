c8af4736d218095a7cbd16fefddeff6d362436d1
ryanbaumann <rsbaumann@gmail.com>
2018-01-27 22:37:11 -0800

    update to 0.1.2 release

--- mapboxgl/__init__.py
+++ mapboxgl/__init__.py
@@ -1,4 +1,4 @@
 from .viz import CircleViz, GraduatedCircleViz, HeatmapViz, ClusteredCircleViz
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ['CircleViz', 'GraduatedCircleViz', 'HeatmapViz', 'ClusteredCircleViz']
