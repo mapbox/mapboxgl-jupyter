5e3e7c1275223fc9b1dbeba7269c1d1dabe3551a
Ryan Baumann <ryanbaumann@users.noreply.github.com>
2018-01-27 22:11:31 -0800

    add cluster to viz init.py, upgrade to 0.44 GL JS (#36)

--- mapboxgl/__init__.py
+++ mapboxgl/__init__.py
@@ -1,4 +1,4 @@
-from .viz import CircleViz, GraduatedCircleViz, HeatmapViz
+from .viz import CircleViz, GraduatedCircleViz, HeatmapViz, ClusteredCircleViz
 
 __version__ = "0.1.1"
 __all__ = ['CircleViz', 'GraduatedCircleViz', 'HeatmapViz', 'ClusteredCircleViz']
--- mapboxgl/viz.py
+++ mapboxgl/viz.py
@@ -6,7 +6,7 @@ from IPython.core.display import HTML, display
 from mapboxgl.errors import TokenError
 from mapboxgl import templates
 
-GL_JS_VERSION = 'v0.43.0'
+GL_JS_VERSION = 'v0.44.0'
 
 
 class CircleViz(object):
