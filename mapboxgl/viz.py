bae0b6d0118546fb81984cb865889a8dbd74db5f
akacarlyann <akacarlyann@gmail.com>
2018-02-02 16:51:11 -0800

    Abstract common Viz properties to MapViz base class; add base.html map template (#37)

--- mapboxgl/viz.py
+++ mapboxgl/viz.py
@@ -9,61 +9,49 @@ from mapboxgl import templates
 GL_JS_VERSION = 'v0.44.0'
 
 
-class CircleViz(object):
-    """Create a circle map"""
+class MapViz(object):
 
     def __init__(self,
                  data,
                  access_token=None,
                  center=(0, 0),
-                 color_property=None,
-                 color_stops=None,
-                 color_default='grey',
-                 color_function_type='interpolate',
-                 label_property=None,
-                 opacity=1,
                  below_layer='',
+                 opacity=1,
                  div_id='map',
                  height='500px',
-                 style_url="mapbox://styles/mapbox/light-v9?optimize=true",
+                 style_url='mapbox://styles/mapbox/light-v9?optimize=true',
                  width='100%',
                  zoom=0):
-        """Construct a Mapviz object
+        """Construct a MapViz object
 
         :param data: GeoJSON Feature Collection
-
-        :param color_property: property to determine circle color
-        :param color_stops: property to determine circle color
-        :param color_default: property to determine default circle color if match lookup fails
-        :param color_function_type: property to determine `type` used by Mapbox to assign color
-
-        :param style_url: url to mapbox style
         :param access_token: Mapbox GL JS access token.
+        :param center: map center point
+        :param style_url: url to mapbox style
         :param div_id: The HTML div id of the map container in the viz
         :param width: The CSS width of the HTML div id in % or pixels.
         :param height: The CSS height of the HTML map div in % or pixels.
+        :param zoom: starting zoom level for map
+        :param opacity: opacity of map data layer
+
         """
         if access_token is None:
             access_token = os.environ.get('MAPBOX_ACCESS_TOKEN', '')
         if not access_token.startswith('pk'):
             raise TokenError('Mapbox access token must be public (pk)')
         self.access_token = access_token
-
+        
+        self.template = 'base'
+        self.data = data
         self.div_id = div_id
         self.width = width
         self.height = height
-        self.data = data
         self.style_url = style_url
         self.center = center
         self.zoom = zoom
-
-        self.color_property = color_property
-        self.color_stops = color_stops
-        self.color_function_type = color_function_type     ####
-        self.color_default = color_default     ####
-        self.label_property = label_property
-        self.opacity = opacity
         self.below_layer = below_layer
+        self.opacity = opacity
+        self.label_property = None
 
     def as_iframe(self, html_data):
         """Build the HTML representation for the mapviz."""
@@ -84,6 +72,9 @@ class CircleViz(object):
         # Display the iframe in the current jupyter notebook view
         display(HTML(map_html))
 
+    def add_unique_template_variables(self, options):
+        pass
+
     def create_html(self):
         """Create a circle visual from a geojson data source"""
         options = dict(
@@ -94,28 +85,65 @@ class CircleViz(object):
             center=list(self.center),
             zoom=self.zoom,
             geojson_data=json.dumps(self.data, ensure_ascii=False),
-            colorProperty=self.color_property,
-            colorType=self.color_function_type, ###
-            colorStops=self.color_stops,
-            defaultColor=self.color_default, ###
-            opacity=self.opacity,
-            belowLayer=self.below_layer)
+            belowLayer=self.below_layer,
+            opacity=self.opacity)
 
         if self.label_property is None:
             options.update(labelProperty=None)
         else:
             options.update(labelProperty='{' + self.label_property + '}')
 
-        return templates.format('circle', **options)
+        self.add_unique_template_variables(options)
+
+        return templates.format(self.template, **options)
 
 
-class GraduatedCircleViz(object):
+class CircleViz(MapViz):
+    """Create a circle map"""
+
+    def __init__(self,
+                 data,
+                 label_property=None,
+                 color_property=None,
+                 color_stops=None,
+                 color_default='grey',
+                 color_function_type='interpolate',
+                 *args,
+                 **kwargs):
+        """Construct a Mapviz object
+
+        :param label_property: property to use for marker label
+        :param color_property: property to determine circle color
+        :param color_stops: property to determine circle color
+        :param color_default: property to determine default circle color if match lookup fails
+        :param color_function_type: property to determine `type` used by Mapbox to assign color
+
+        """
+        super(CircleViz, self).__init__(data, *args, **kwargs)
+
+        self.template = 'circle'
+        self.label_property = label_property
+        self.color_property = color_property
+        self.color_stops = color_stops
+        self.color_function_type = color_function_type
+        self.color_default = color_default
+
+    def add_unique_template_variables(self, options):
+        """Update map template variables specific to circle visual"""
+        options.update(dict(
+            geojson_data=json.dumps(self.data, ensure_ascii=False),
+            colorProperty=self.color_property,
+            colorType=self.color_function_type,
+            colorStops=self.color_stops,
+            defaultColor=self.color_default
+        ))
+
+
+class GraduatedCircleViz(MapViz):
     """Create a graduated circle map"""
 
     def __init__(self,
                  data,
-                 access_token=None,
-                 center=(0, 0),
                  label_property=None,
                  color_property=None,
                  color_stops=None,
@@ -125,17 +153,11 @@ class GraduatedCircleViz(object):
                  radius_stops=None,
                  radius_default=None,
                  radius_function_type='interpolate',
-                 opacity=1,
-                 below_layer='',
-                 div_id='map',
-                 height='500px',
-                 style_url="mapbox://styles/mapbox/light-v9?optimize=true",
-                 width='100%',
-                 zoom=0):
+                 *args,
+                 **kwargs):
         """Construct a Mapviz object
 
-        :param data: GeoJSON Feature Collection
-
+        :param label_property: property to use for marker label
         :param color_property: property to determine circle color
         :param color_stops: property to determine circle color
         :param color_default: property to determine default circle color if match lookup fails
@@ -145,69 +167,23 @@ class GraduatedCircleViz(object):
         :param radius_default: property to determine default circle radius if match lookup fails
         :param radius_function_type: property to determine `type` used by Mapbox to assign radius size
 
-        :param style_url: url to mapbox style
-        :param access_token: Mapbox GL JS access token.
-        :param div_id: The HTML div id of the map container in the viz
-        :param width: The CSS width of the HTML div id in % or pixels.
-        :param height: The CSS height of the HTML map div in % or pixels.
         """
-        if access_token is None:
-            access_token = os.environ.get('MAPBOX_ACCESS_TOKEN', '')
-        if not access_token.startswith('pk'):
-            raise TokenError('Mapbox access token must be public (pk)')
-        self.access_token = access_token
+        super(GraduatedCircleViz, self).__init__(data, *args, **kwargs)
 
-        self.div_id = div_id
-        self.width = width
-        self.height = height
-        self.data = data
-        self.style_url = style_url
-        self.center = center
-        self.zoom = zoom
-
+        self.template = 'graduated_circle'
+        self.label_property = label_property
         self.color_property = color_property
         self.color_stops = color_stops
         self.radius_property = radius_property
         self.radius_stops = radius_stops
-
         self.color_function_type = color_function_type
         self.color_default = color_default
         self.radius_function_type = radius_function_type
         self.radius_default = radius_default
 
-        self.label_property = label_property
-        self.opacity = opacity
-        self.below_layer = below_layer
-
-    def as_iframe(self, html_data):
-        """Build the HTML representation for the mapviz."""
-
-        srcdoc = html_data.replace('"', "'")
-        return ('<iframe id="{div_id}", srcdoc="{srcdoc}" style="width: {width}; '
-                'height: {height};"></iframe>'.format(
-                    div_id=self.div_id,
-                    srcdoc=srcdoc,
-                    width=self.width,
-                    height=self.height))
-
-    def show(self, **kwargs):
-        # Load the HTML iframe
-        html = self.create_html(**kwargs)
-        map_html = self.as_iframe(html)
-
-        # Display the iframe in the current jupyter notebook view
-        display(HTML(map_html))
-
-    def create_html(self):
-        """Create a circle visual from a geojson data source"""
-        options = dict(
-            gl_js_version=GL_JS_VERSION,
-            accessToken=self.access_token,
-            div_id=self.div_id,
-            styleUrl=self.style_url,
-            center=list(self.center),
-            zoom=self.zoom,
-            geojson_data=json.dumps(self.data, ensure_ascii=False),
+    def add_unique_template_variables(self, options):
+        """Update map template variables specific to graduated circle visual"""
+        options.update(dict(
             colorProperty=self.color_property,
             colorStops=self.color_stops,
             colorType=self.color_function_type, 
@@ -216,199 +192,79 @@ class GraduatedCircleViz(object):
             defaultRadius=self.radius_default, 
             radiusProperty=self.radius_property,
             radiusStops=self.radius_stops,
-            opacity=self.opacity,
-            belowLayer=self.below_layer)
-
-        if self.label_property is None:
-            options.update(labelProperty=None)
-        else:
-            options.update(labelProperty='{' + self.label_property + '}')
-
-        return templates.format('graduated_circle', **options)
+        ))
 
 
-class HeatmapViz(object):
+class HeatmapViz(MapViz):
     """Create a heatmap viz"""
 
     def __init__(self,
                  data,
-                 access_token=None,
-                 center=(0, 0),
                  weight_property=None,
                  weight_stops=None,
                  color_stops=None,
                  radius_stops=None,
-                 opacity=1,
-                 below_layer='',
-                 div_id='map',
-                 height='500px',
-                 style_url="mapbox://styles/mapbox/light-v9?optimize=true",
-                 width='100%',
-                 zoom=0):
+                 *args,
+                 **kwargs):
         """Construct a Mapviz object
-
-        :param data: GeoJSON Feature Collection
 
         :param weight_property: property to determine heatmap weight. EX. "population"
         :param weight_stops: stops to determine heatmap weight.  EX. [[10, 0], [100, 1]]
         :param color_stops: stops to determine heatmap color.  EX. [[0, "red"], [0.5, "blue"], [1, "green"]]
         :param radius_stops: stops to determine heatmap radius based on zoom.  EX: [[0, 1], [12, 30]]
 
-        :param style_url: url to mapbox style
-        :param access_token: Mapbox GL JS access token.
-        :param div_id: The HTML div id of the map container in the viz
-        :param width: The CSS width of the HTML div id in % or pixels.
-        :param height: The CSS height of the HTML map div in % or pixels.
         """
-        if access_token is None:
-            access_token = os.environ.get('MAPBOX_ACCESS_TOKEN', '')
-        if not access_token.startswith('pk'):
-            raise TokenError('Mapbox access token must be public (pk)')
-        self.access_token = access_token
+        super(HeatmapViz, self).__init__(data, *args, **kwargs)
 
-        self.div_id = div_id
-        self.width = width
-        self.height = height
-        self.data = data
-        self.style_url = style_url
-        self.center = center
-        self.zoom = zoom
-
+        self.template = 'heatmap'
         self.weight_property = weight_property
         self.weight_stops = weight_stops
         self.color_stops = color_stops
         self.radius_stops = radius_stops
-        self.opacity = opacity
-        self.below_layer = below_layer
 
-    def as_iframe(self, html_data):
-        """Build the HTML representation for the mapviz."""
-
-        srcdoc = html_data.replace('"', "'")
-        return ('<iframe id="{div_id}", srcdoc="{srcdoc}" style="width: {width}; '
-                'height: {height};"></iframe>'.format(
-                    div_id=self.div_id,
-                    srcdoc=srcdoc,
-                    width=self.width,
-                    height=self.height))
-
-    def show(self, **kwargs):
-        # Load the HTML iframe
-        html = self.create_html(**kwargs)
-        map_html = self.as_iframe(html)
-
-        # Display the iframe in the current jupyter notebook view
-        display(HTML(map_html))
-
-    def create_html(self):
-        """Create a circle visual from a geojson data source"""
-        options = dict(
-            gl_js_version=GL_JS_VERSION,
-            accessToken=self.access_token,
-            div_id=self.div_id,
-            styleUrl=self.style_url,
-            center=list(self.center),
-            zoom=self.zoom,
-            geojson_data=json.dumps(self.data, ensure_ascii=False),
+    def add_unique_template_variables(self, options):
+        """Update map template variables specific to heatmap visual"""
+        options.update(dict(
             colorStops=self.color_stops,
             radiusStops=self.radius_stops,
             weightProperty=self.weight_property,
-            weightStops=self.weight_stops,
-            opacity=self.opacity,
-            belowLayer=self.below_layer)
-
-        return templates.format('heatmap', **options)
+            weightStops=self.weight_stops
+        ))
 
 
-class ClusteredCircleViz(object):
+class ClusteredCircleViz(MapViz):
     """Create a clustered circle map"""
 
     def __init__(self,
                  data,
-                 access_token=None,
-                 center=(0, 0),
                  color_stops=None,
                  radius_stops=None,
                  cluster_radius=30,
                  cluster_maxzoom=14,
-                 opacity=1,
-                 below_layer='',
-                 div_id='map',
-                 height='500px',
-                 style_url="mapbox://styles/mapbox/light-v9?optimize=true",
-                 width='100%',
-                 zoom=0):
+                 *args,
+                 **kwargs):
         """Construct a Mapviz object
-
-        :param data: GeoJSON Feature Collection
 
         :param color_property: property to determine circle color
         :param color_stops: property to determine circle color
         :param radius_property: property to determine circle radius
         :param radius_stops: property to determine circle radius
 
-        :param style_url: url to mapbox style
-        :param access_token: Mapbox GL JS access token.
-        :param div_id: The HTML div id of the map container in the viz
-        :param width: The CSS width of the HTML div id in % or pixels.
-        :param height: The CSS height of the HTML map div in % or pixels.
         """
-        if access_token is None:
-            access_token = os.environ.get('MAPBOX_ACCESS_TOKEN', '')
-        if not access_token.startswith('pk'):
-            raise TokenError('Mapbox access token must be public (pk)')
-        self.access_token = access_token
+        super(ClusteredCircleViz, self).__init__(data, *args, **kwargs)
 
-        self.div_id = div_id
-        self.width = width
-        self.height = height
-        self.data = data
-        self.style_url = style_url
-        self.center = center
-        self.zoom = zoom
-
+        self.template = 'clustered_circle'
         self.color_stops = color_stops
         self.radius_stops = radius_stops
-        self.opacity = opacity
-        self.below_layer = below_layer
         self.clusterRadius = cluster_radius
         self.clusterMaxZoom = cluster_maxzoom
 
-    def as_iframe(self, html_data):
-        """Build the HTML representation for the mapviz."""
-
-        srcdoc = html_data.replace('"', "'")
-        return ('<iframe id="{div_id}", srcdoc="{srcdoc}" style="width: {width}; '
-                'height: {height};"></iframe>'.format(
-                    div_id=self.div_id,
-                    srcdoc=srcdoc,
-                    width=self.width,
-                    height=self.height))
-
-    def show(self, **kwargs):
-        # Load the HTML iframe
-        html = self.create_html(**kwargs)
-        map_html = self.as_iframe(html)
-
-        # Display the iframe in the current jupyter notebook view
-        display(HTML(map_html))
-
-    def create_html(self):
-        """Create a circle visual from a geojson data source"""
-        options = dict(
-            gl_js_version=GL_JS_VERSION,
-            accessToken=self.access_token,
-            div_id=self.div_id,
-            styleUrl=self.style_url,
-            center=list(self.center),
-            zoom=self.zoom,
-            geojson_data=json.dumps(self.data, ensure_ascii=False),
+    def add_unique_template_variables(self, options):
+        """Update map template variables specific to a clustered circle visual"""
+        options.update(dict(
             colorStops=self.color_stops,
             baseColor=self.color_stops[0][1],
             radiusStops=self.radius_stops,
             clusterRadius=self.clusterRadius,
-            clusterMaxZoom=self.clusterMaxZoom,
-            opacity=self.opacity,
-            belowLayer=self.below_layer)
-
-        return templates.format('clustered_circle', **options)
+            clusterMaxZoom=self.clusterMaxZoom
+        ))
