7d94140a14685bcf6eea08e6b40b4ae4e41505e2
ryanbaumann <rsbaumann@gmail.com>
2018-02-18 20:07:46 -0800

    fixed bugs in new geojson export from DF

--- mapboxgl/utils.py
+++ mapboxgl/utils.py
@@ -1,33 +1,37 @@
 from .colors import color_ramps
 import geojson
+import json
 
 
 def row_to_geojson(row, lon, lat):
-    """Convert a pandas dataframe row to a geojson format object
+    """Convert a pandas dataframe row to a geojson format object.  Converts all datetimes to epoch seconds.
     """
-    row_json = row.to_dict() #Let pandas handle json serialization
+
+    # Let pandas handle json serialization
+    row_json = json.loads(row.to_json(date_format='epoch', date_unit='s'))
     return geojson.Feature(geometry=geojson.Point((row_json[lon], row_json[lat])),
-                           properties={ key: row_json[key] for key in row_json.keys() if key not in [lon, lat] })
+                           properties={key: row_json[key] for key in row_json.keys() if key not in [lon, lat]})
 
 
 def df_to_geojson(df, properties=None, lat='lat', lon='lon', precision=None, filename=None):
     """Serialize a Pandas dataframe to a geojson format Python dictionary
     """
-
-    for prop in properties:
-        # Check if list of properties exists in dataframe columns
-        if prop not in list(df.columns.values):
-            raise ValueError('properties must be a valid list of column names from dataframe')
-        if prop in [lon, lat]:
-            raise ValueError('properties cannot be the geometry longitude or latitude column')
+    if precision:
+        df[lon] = df[lon].round(precision)
+        df[lat] = df[lat].round(precision)
 
     if not properties:
         # if no properties are selected, use all properties in dataframe
         properties = [c for c in df.columns if c not in [lon, lat]]
 
-    if precision:
-        df[lat] = df[lat].round(precision)
-        df[lon] = df[lon].round(precision)
+    for prop in properties:
+        # Check if list of properties exists in dataframe columns
+        if prop not in list(df.columns.values):
+            raise ValueError(
+                'properties must be a valid list of column names from dataframe')
+        if prop in [lon, lat]:
+            raise ValueError(
+                'properties cannot be the geometry longitude or latitude column')
 
     if filename:
         with open(filename, 'w+') as f:
@@ -35,25 +39,28 @@ def df_to_geojson(df, properties=None, lat='lat', lon='lon', precision=None, fil
             pass
 
         with open(filename, 'a+') as f:
-            features = df[ [lon, lat] + properties ].apply(lambda x: row_to_geojson(
-                x, lon, lat), axis=1)
+            features = []
+            df[[lon, lat] + properties].apply(lambda x: features.append(
+                row_to_geojson(x, lon, lat)), axis=1)
+
             f.write('{"type": "FeatureCollection", "features": [\n')
             for idx, feat in enumerate(features):
-                if idx ==0:
-                    f.write(geojson.dumps(features[0]))
+                if idx == 0:
+                    f.write(geojson.dumps(feat) + '\n')
                 else:
-                    f.write(',' + geojson.dumps(feat))
+                    f.write(',' + geojson.dumps(feat) + '\n')
             f.write(']}')
+
             return {
                 "type": "file",
                 "filename": filename,
                 "feature_count": len(features)
             }
     else:
-        return geojson.featureCollection(df[ [lon, lat] + properties ].apply(lambda x: row_to_geojson(
-                                                  x, lon, lat),
-                                                  axis=1)
-                                         )
+        features = []
+        df[[lon, lat] + properties].apply(lambda x: features.append(
+            row_to_geojson(x, lon, lat)), axis=1)
+        return geojson.FeatureCollection(features)
 
 
 def scale_between(minval, maxval, numStops):
