70c95ea5db6b6193622f1dd8f7788b59ff7f6db8
ryanbaumann <rsbaumann@gmail.com>
2018-02-23 07:44:16 -0800

    version bump 0.5.1

--- mapboxgl/__init__.py
+++ mapboxgl/__init__.py
@@ -1,4 +1,4 @@
 from .viz import CircleViz, GraduatedCircleViz, HeatmapViz, ClusteredCircleViz
 
-__version__ = "0.1.4"
+__version__ = "0.5.1"
 __all__ = ['CircleViz', 'GraduatedCircleViz', 'HeatmapViz', 'ClusteredCircleViz']
