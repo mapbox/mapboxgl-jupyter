ac8c1000833e56cd7d806c56894e5df2f694aa45
akacarlyann <akacarlyann@gmail>
2018-02-27 20:53:31 -0800

    Add tests for ChoroplethViz (currently uses polygons.geojson)

--- tests/test_html.py
+++ tests/test_html.py
@@ -13,6 +13,12 @@ def data():
         return json.loads(fh.read())
 
 
+@pytest.fixture()
+def polygon_data():
+    with open('tests/polygons.geojson') as fh:
+        return json.loads(fh.read())
+
+
 TOKEN = 'pk.abc123'
 
 
@@ -32,11 +38,37 @@ def test_secret_key_GraduatedCircleViz(data):
         GraduatedCircleViz(data, access_token=secret)
 
 
+def test_secret_key_ChoroplethViz(polygon_data):
+    """Secret key raises a token error
+    """
+    secret = 'sk.abc123'
+    with pytest.raises(TokenError):
+        ChoroplethViz(polygon_data, access_token=secret)
+
+
 def test_token_env_CircleViz(monkeypatch, data):
     """Viz can get token from environment if not specified
     """
     monkeypatch.setenv('MAPBOX_ACCESS_TOKEN', TOKEN)
     viz = CircleViz(data, color_property="Avg Medicare Payments")
+    assert TOKEN in viz.create_html()
+
+
+def test_token_env_GraduatedCircleViz(monkeypatch, data):
+    """Viz can get token from environment if not specified
+    """
+    monkeypatch.setenv('MAPBOX_ACCESS_TOKEN', TOKEN)
+    viz = GraduatedCircleViz(data,
+                             color_property="Avg Medicare Payments",
+                             radius_property="Avg Covered Charges")
+    assert TOKEN in viz.create_html()
+
+
+def test_token_env_ChoroplethViz(monkeypatch, polygon_data):
+    """Viz can get token from environment if not specified
+    """
+    monkeypatch.setenv('MAPBOX_ACCESS_TOKEN', TOKEN)
+    viz = ChoroplethViz(polygon_data, color_property="density")
     assert TOKEN in viz.create_html()
 
 
@@ -55,14 +87,12 @@ def test_html_GraduatedCricleViz(data):
     assert "<html>" in viz.create_html()
 
 
-def test_token_env_GraduatedCircleViz(monkeypatch, data):
-    """Viz can get token from environment if not specified
-    """
-    monkeypatch.setenv('MAPBOX_ACCESS_TOKEN', TOKEN)
-    viz = GraduatedCircleViz(data,
-                             color_property="Avg Medicare Payments",
-                             radius_property="Avg Covered Charges")
-    assert TOKEN in viz.create_html()
+def test_html_ChoroplethViz(polygon_data):
+    viz = ChoroplethViz(polygon_data,
+                        color_property="density",
+                        color_stops=[[0.0, "red"], [50.0, "gold"], [1000.0, "blue"]],
+                        access_token=TOKEN)
+    assert "<html>" in viz.create_html()
 
 
 @patch('mapboxgl.viz.display')
@@ -103,6 +133,7 @@ def test_display_HeatmapViz(display, data):
     viz.show()
     display.assert_called_once()
 
+
 @patch('mapboxgl.viz.display')
 def test_display_ClusteredCircleViz(display, data):
     """Assert that show calls the mocked display function
@@ -114,6 +145,19 @@ def test_display_ClusteredCircleViz(display, data):
     viz.show()
     display.assert_called_once()
 
+
+@patch('mapboxgl.viz.display')
+def test_display_ChoroplethViz(display, polygon_data):
+    """Assert that show calls the mocked display function
+    """
+    viz = ChoroplethViz(polygon_data,
+                        color_property="density",
+                        color_stops=[[0.0, "red"], [50.0, "gold"], [1000.0, "blue"]],
+                        access_token=TOKEN)
+    viz.show()
+    display.assert_called_once()
+
+
 @patch('mapboxgl.viz.display')
 def test_min_zoom(display, data):
     viz = GraduatedCircleViz(data,
@@ -124,6 +168,7 @@ def test_min_zoom(display, data):
                              min_zoom=10)
     viz.show()
     display.assert_called_once()
+
 
 @patch('mapboxgl.viz.display')
 def test_max_zoom(display, data):
