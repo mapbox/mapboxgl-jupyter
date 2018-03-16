d3887db29623d22bfd7369d0e142bc4179941ced
vincentsarago <vincent.sarago@mapbox.com>
2018-03-16 10:59:26 -0400

    doc

--- mapboxgl/viz.py
+++ mapboxgl/viz.py
@@ -297,7 +297,7 @@ class ImageViz(MapViz):
 
         :param coordinates: property to determine image coordinates (UL, UR, LR, LL).
             EX. [[-80.425, 46.437], [-71.516, 46.437], [-71.516, 37.936], [-80.425, 37.936]]
-        :param image: image url or base64 encoded.  EX. "https://www.mapbox.com/mapbox-gl-js/assets/radar.gif"]
+        :param image: url, local path or a numpy ndarray
 
         """
         super(ImageViz, self).__init__(None, *args, **kwargs)
