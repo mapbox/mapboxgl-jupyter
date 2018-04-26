f7be72f27c042a3939b6c78fe304a0ca6caecd8d
ryanbaumann <rsbaumann@gmail.com>
2018-04-26 19:35:03 +0100

    bump to 0.7.2 tag

--- mapboxgl/__init__.py
+++ mapboxgl/__init__.py
@@ -1,4 +1,4 @@
 from .viz import CircleViz, GraduatedCircleViz, HeatmapViz, ClusteredCircleViz, ImageViz, RasterTilesViz, ChoroplethViz, LinestringViz
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 __all__ = ['CircleViz', 'GraduatedCircleViz', 'HeatmapViz', 'ClusteredCircleViz', 'ImageViz', 'RasterTilesViz', 'ChoroplethViz', 'LinestringViz']
